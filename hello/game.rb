7c027d9bf8cc7a79bccbaf843c187d4a62fd79f4
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-12-01 04:56:54 -0800

    first full draft of game.rb

--- hello/game.rb
+++ hello/game.rb
@@ -1,16 +1,15 @@
 require 'sinatra'
-
-# before we process a route, we'll se the response as 
-# plain text and set up an array of viable moves that
+# before we process a route, we'll set the response as
+# plain text and set up and array of viable moves that
 # a player (and the computer) can perform
 before do
   content_type :txt
-  @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
-  @throws = @defeat.keys
+  @default = {rock: :scissors, paper: :rock, scissors: :paper}
+  @throws = @default.keys
 end
 
 get '/throw/:type' do
-  # the params[] hash stores querystring and from data.
+  # the params[] hash sotres querystring and form data.
   player_throw = params[:type].to_sym
 
   # in the case of a player providing a throw that is not valid,
@@ -18,5 +17,17 @@ get '/throw/:type' do
   # know they need to make a valid throw to play.
   if !@throws.include?(player_throw)
     halt 403, "You must throw one of the following: #{@throws}"
+  end
+
+  # now we can select a random throw for the computer
+  computer_throw = @throws.sample
+
+  # compare the player and computer throws to determine a winner
+  if player_throw == computer_throw
+    "You tied with the computer. Try again!"
+  elsif computer_throw == @defeat[player_throw]
+    "Nicely done; #{player_throw} beats #{computer_throw}"
+  else 
+    "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
   end
 end
